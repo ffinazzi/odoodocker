fd776398db7cc37c5df8980e674ec4072ab35d8b
ffinazzi <82581020+ffinazzi@users.noreply.github.com>
2021-04-15 16:44:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db-postgresql-fra1-63198-do-user-9008599-0.b.db.ondigitalocean.com'}}
+: ${PORT:=${DB_PORT_5432_TCP_PORT:=25060}}
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='qaheuyhl9gqyp9j9'}}}
 
 DB_ARGS=()
 function check_config() {
